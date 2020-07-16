3e7756258bb117d934c7349094adb203d122108d
Andrey Starodubtsev <andreystar2403@gmail.com>
2020-07-16 14:51:20 +0300

    Add image selector for gallery.

--- andreystar/portfolio/src/main/webapp/script.js
+++ andreystar/portfolio/src/main/webapp/script.js
@@ -1,11 +1,11 @@
 function addRandomQuote() {
-  const quotes =
-    ['Now. Say my name. Heisenberg. You\'re god damn right',
-      'I am the danger.',
-      'You see, but you do not observe.',
-      'There’s a woman lying dead. Perfectly sound analysis but I was hoping you’d go deeper.',
-      'You\'re treading on some mighty thin ice here'
-    ];
+  const quotes = [
+    'Now. Say my name. Heisenberg. You\'re god damn right',
+    'I am the danger.',
+    'You see, but you do not observe.',
+    'There’s a woman lying dead. Perfectly sound analysis but I was hoping you’d go deeper.',
+    'You\'re treading on some mighty thin ice here.'
+  ];
 
   // Pick a random quote.
   const quote = quotes[Math.floor(Math.random() * quotes.length)];
@@ -13,4 +13,31 @@ function addRandomQuote() {
   // Add it to the page.
   const quoteContainer = document.getElementById('quote-container');
   quoteContainer.innerText = quote;
+}
+
+// Image gallery
+const imageSources = [
+  'senya-gorgeous.jpg',
+  'senya-superman.jpg',
+  'senya-dissapointed.jpg'
+];
+let catImage;
+let imageSelector;
+
+function setPhoto() {
+  for (let selector of imageSelector) {
+    if (selector.checked) {
+      catImage.src = 'images/' + imageSources[selector.value];
+      break;
+    }
+  }
+}
+
+window.onload = function() {
+  catImage = document.getElementById("cat-photo");
+  imageSelector = document.getElementsByName("cat-photo-id");
+  for (let selector of imageSelector) {
+    selector.onchange = setPhoto;
+  }
+  setPhoto();
 }
