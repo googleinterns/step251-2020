50ef021ffadd87d5c574b089e2b8d95c8a39895a
Anca <anca.rosculete@gmail.com>
2020-07-22 08:00:40 +0000

    Show all the existing comments on the page from Datastore.

--- ancar/portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ ancar/portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -17,14 +17,16 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
@@ -32,7 +34,21 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      //TODO(ancar): load comments when an user requests it
+      Query query = new Query("Comment");
+
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      PreparedQuery results = datastore.prepare(query);
+
+      List<String> comments = new ArrayList<>();
+      for (Entity entity : results.asIterable()) {
+        String comment = (String) entity.getProperty("comment");
+        comments.add(comment);
+      }
+
+      Gson gson = new Gson();
+
+      response.setContentType("application/json;");
+      response.getWriter().println(gson.toJson(comments));
   }
 
   @Override
