38b85ea52b3673aa2b1d27f01b4d1bf0cdfc9aeb
Anca <anca.rosculete@gmail.com>
2020-07-30 08:20:24 +0000

    Add the suggestions regarding the MapServlet.

--- ancar/portfolio/src/main/java/com/google/sps/servlets/MapServlet.java
+++ ancar/portfolio/src/main/java/com/google/sps/servlets/MapServlet.java
@@ -3,6 +3,7 @@ package com.google.sps.servlets;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,11 +14,11 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/map-data")
 public class MapServlet extends HttpServlet {
 
-    private class elementMap {
+    private static class ElementMap {
         double lat, lng;
         String name, description;
 
-        public elementMap(double lat, double lng, String name, String description) {
+        public ElementMap(double lat, double lng, String name, String description) {
             this.lat = lat;
             this.lng = lng;
             this.name = name;
@@ -25,30 +26,30 @@ public class MapServlet extends HttpServlet {
         }
     }
 
-    private ArrayList<elementMap> markers;
+    private List<ElementMap> markers;
+    private static final Gson GSON = new Gson();
 
     @Override
     public void init() {
-        markers = new ArrayList<elementMap>();
+        markers = new ArrayList<>();
 
-        Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/cities.csv"));
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] cells = line.split(",");
+        try(Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/cities.csv"))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] cells = line.split(",");
 
-            double lat = Double.parseDouble(cells[0]);
-            double lng = Double.parseDouble(cells[1]);
+                double lat = Double.parseDouble(cells[0]);
+                double lng = Double.parseDouble(cells[1]);
 
-            markers.add(new elementMap(lat, lng, cells[2], cells[3]));
+                markers.add(new ElementMap(lat, lng, cells[2], cells[3]));
+            }
+            scanner.close();
         }
-        scanner.close();
     }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
-        Gson gson = new Gson();
-        String json = gson.toJson(markers);
-        response.getWriter().println(json);
+        response.getWriter().println(GSON.toJson(markers));
     }
 }
