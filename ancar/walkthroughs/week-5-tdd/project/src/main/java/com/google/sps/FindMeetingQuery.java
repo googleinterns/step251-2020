cb8aa08fbea3b681c06abf933c5924f51cbf9ed1
Anca <anca.rosculete@gmail.com>
2020-07-31 07:33:37 +0000

    Remove last commits pushed on wrong PR.

--- ancar/walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ ancar/walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -20,14 +20,24 @@ import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+      Collection<TimeRange> meetingQueriesMandatoryPeople = getSlotsMandatoryAttendees(events, request);
+      Collection<TimeRange> meetingQueriesOptionalPeople = getSlotsWithOptionalAttendees(events, request);
+
+      if (meetingQueriesOptionalPeople.isEmpty()) {
+          return meetingQueriesMandatoryPeople;
+      } else {
+          return meetingQueriesOptionalPeople;
+      }
+  }
+
+  public Collection<TimeRange> getSlotsMandatoryAttendees(Collection<Event> events, MeetingRequest request) {
     List<TimeRange> meetingQueries = new ArrayList<>();
     int startTime = -1;
     int intervalForMeet = 0;
     boolean approvedTimeRange;
 
-
     for (int hour = 0; hour <= 23; hour++) {
-        for (int minute = 0; minute <=59; minute ++) {
+        for (int minute = 0; minute <= 59; minute ++) {
             approvedTimeRange = true;
             TimeRange possibleTimeRange = TimeRange.fromStartDuration(hour * 60 + minute, (int)request.getDuration());
 
@@ -36,6 +46,51 @@ public final class FindMeetingQuery {
                     if (event.getWhen().overlaps(possibleTimeRange)) {
                         for (String attendee : event.getAttendees()) {
                             if (request.getAttendees().contains(attendee)) {
+                                approvedTimeRange = false;
+                            }
+                        }
+                    }
+                }
+
+                if (approvedTimeRange) {
+                    if (startTime == -1) {
+                        startTime = possibleTimeRange.start();
+                        intervalForMeet = 0;
+                    } else {
+                        intervalForMeet++;
+                    }
+                } else if (startTime != -1) {
+                    meetingQueries.add(TimeRange.fromStartDuration(startTime, (int) (request.getDuration() + intervalForMeet)));
+                    startTime = -1;
+                }
+            }
+        }
+    }
+
+    if (startTime != -1) {
+        meetingQueries.add(TimeRange.fromStartDuration(startTime, (int) (request.getDuration() + intervalForMeet)));
+    }
+
+    return meetingQueries;
+  }
+
+  public Collection<TimeRange> getSlotsWithOptionalAttendees(Collection<Event> events, MeetingRequest request) {
+    List<TimeRange> meetingQueries = new ArrayList<>();
+    int startTime = -1;
+    int intervalForMeet = 0;
+    boolean approvedTimeRange;
+
+    for (int hour = 0; hour <= 23; hour++) {
+        for (int minute = 0; minute <= 59; minute ++) {
+            approvedTimeRange = true;
+            TimeRange possibleTimeRange = TimeRange.fromStartDuration(hour * 60 + minute, (int)request.getDuration());
+
+            if (TimeRange.WHOLE_DAY.contains(possibleTimeRange)) {
+                for (Event event : events) {
+                    if (event.getWhen().overlaps(possibleTimeRange)) {
+                        for (String attendee : event.getAttendees()) {
+                            if (request.getAttendees().contains(attendee) || 
+                                request.getOptionalAttendees().contains(attendee)) {
                                 approvedTimeRange = false;
                             }
                         }
