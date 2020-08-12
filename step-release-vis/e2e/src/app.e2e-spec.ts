12a2728c29ea88c9253c55477bed0ac0989cc76c
Andrey Starodubtsev <andreystar2403@gmail.com>
2020-08-12 18:20:36 +0300

    Update test files for e2e.

--- step-release-vis/e2e/src/app.e2e-spec.ts
+++ step-release-vis/e2e/src/app.e2e-spec.ts
@@ -9,11 +9,6 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('step-release-vis app is running!');
-  });
-
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
