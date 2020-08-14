069481e264385921e2d0b0c6d36029d43f8da440
Andrey Starodubtsev <andreystar2403@gmail.com>
2020-08-14 19:04:08 +0300

    Update testing paths and fix imports in tests.

--- step-release-vis/src/app/services/environment_test.ts
+++ step-release-vis/src/app/services/environment_test.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EnvironmentService } from './environment';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('EnvironmentService', () => {
   let service: EnvironmentService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(EnvironmentService);
   });
 
--- step-release-vis/src/test.ts
+++ step-release-vis/src/test.ts
@@ -21,6 +21,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+const context = require.context('./', true, /\.spec\.ts$|_test\.ts$/);
 // And load the modules.
 context.keys().map(context);
