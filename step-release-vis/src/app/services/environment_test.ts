7f4aec68d4aa6d8cdda3756fc3ebb21268c20ae7
Anca-Maria Roşculete <ancar@google.com>
2020-08-17 16:16:32 +0000

    Add test for empty list and modify test for fractional percentages.

--- step-release-vis/src/app/services/environment_test.ts
+++ step-release-vis/src/app/services/environment_test.ts
@@ -22,14 +22,14 @@ describe('EnvironmentService', () => {
 
   // TODO(naoai): write getPolygons test
   it('#getPercentages should return 3 candidates with ~ equal percentages', () => {
-    const input: CandidateInfo[] = [{name:  '1', job_count: 666}, {name: '2', job_count: 667},
-      {name: '3', job_count: 667}];
+    const input: CandidateInfo[] = [{name:  '1', job_count: 105}, {name: '2', job_count: 300},
+      {name: '3', job_count: 595}];
     // @ts-ignore
     const resultMap: Map<string, number> = service.getPercentages(input);
 
-    expect(fractionalEqual(resultMap.get('1'), 33.3)).toBe(true);
-    expect(fractionalEqual(resultMap.get('2'), 33.35)).toBe(true);
-    expect(fractionalEqual(resultMap.get('3'), 33.35)).toBe(true);
+    expect(resultMap.get('1')).toEqual(10.5);
+    expect(resultMap.get('2')).toEqual(30);
+    expect(resultMap.get('3')).toEqual(59.5);
   });
 
   it('#getPercentages should have a candidate with 100', () => {
@@ -38,5 +38,12 @@ describe('EnvironmentService', () => {
     const resultMap: Map<string, number> = service.getPercentages(input);
 
     expect(resultMap.get('1')).toEqual(100);
+  });
+
+  it('#getPercenatges with empty CanditateInfo[]', () => {
+    // @ts-ignore
+    const resultMap: Map<string, number> = service.getPercentages([]);
+
+    expect(resultMap).toBe([]);
   });
 });
