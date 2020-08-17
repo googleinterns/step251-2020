792394a9c694ee7a8477b874a05790550ce51654
Laura Georgescu <naoai@google.com>
2020-08-17 17:15:43 +0000

    Implement function to insert and update snapshots.

--- step-release-vis/src/app/services/environment.ts
+++ step-release-vis/src/app/services/environment.ts
@@ -24,6 +24,35 @@ export class EnvironmentService {
     return [];
   }
 
+  private insertAndAdjustPoints(candsInfo: CandidateInfo[], set: TimestampUpperBoundSet): [TimestampUpperBoundSet, number] {
+    const percentages = this.getPercentages(candsInfo);
+    const newSet: TimestampUpperBoundSet = set;
+
+    for (let i = 0; i < set.snapshot.length; i++) {
+      if (percentages.has(set.snapshot[i].candName)) { // still exists
+        newSet.snapshot[i].position = percentages.get(set.snapshot[i].candName);
+      } else { // will be erased
+        newSet.snapshot[i].position = 0;
+      }
+    }
+
+    let newCandidates = 0;
+    for (const entry of percentages.entries()) {
+      if (!set.orderMap.has(entry[0])) { // the candidate has to be introduced
+        newCandidates++;
+        newSet.orderMap.set(entry[0], newSet.snapshot.length);
+        newSet.snapshot.push(new PolygonUpperBoundYPosition(entry[0], entry[1]));
+      }
+    }
+
+    let sum = 0;
+    for (const candidate of newSet.snapshot) {
+      sum += candidate.position;
+      candidate.position = sum;
+    }
+    return [newSet, newCandidates];
+  }
+
   private getPercentages(candsInfo: CandidateInfo[]): Map<string, number> {
     const candInfo2percentage: Map<string, number> = new Map();
     let totalJobSum = 0;
@@ -42,5 +71,27 @@ export class EnvironmentService {
 
   private readJson(jsonFile: string): Observable<Environment[]> {
     return this.fileService.readContents<Environment[]>(jsonFile);
+  }
+}
+
+class TimestampUpperBoundSet {
+  /* What is the order of the polygons? */
+  orderMap: Map<string, number>;
+  /* Store the details about each _active_ polygon */
+  snapshot: PolygonUpperBoundYPosition[];
+
+  constructor() {
+    this.orderMap = new Map();
+    this.snapshot = [];
+  }
+}
+
+class PolygonUpperBoundYPosition {
+  candName: string;
+  position: number;
+
+  constructor(name: string, pos: number) {
+    this.candName = name;
+    this.position = pos;
   }
 }
