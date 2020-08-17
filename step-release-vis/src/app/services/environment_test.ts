f6f5997a5770e4c4dbad33b49cda6511bf91a5d9
Anca-Maria Roşculete <ancar@google.com>
2020-08-17 14:18:12 +0000

    Add 2 tests for getPercentages.

--- step-release-vis/src/app/services/environment_test.ts
+++ step-release-vis/src/app/services/environment_test.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { EnvironmentService } from './environment';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {CandidateInfo} from '../models/Data';
 
 describe('EnvironmentService', () => {
   let service: EnvironmentService;
@@ -13,10 +14,24 @@ describe('EnvironmentService', () => {
     service = TestBed.inject(EnvironmentService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-
   // TODO(naoai): write getPolygons test
   // TODO(ancar): test getPercentages
+  it('#getPercentages should return 3 candidates with ~ equal percentages', () => {
+    const input: CandidateInfo[] = [{name:  '1', job_count: 666}, {name: '2', job_count: 667},
+      {name: '3', job_count: 667}];
+    // @ts-ignore
+    const resultMap: Map<string, number> = service.getPercentages(input);
+
+    expect(resultMap.get('1')).toEqual(34);
+    expect(resultMap.get('2')).toEqual(33);
+    expect(resultMap.get('3')).toEqual(33);
+  });
+
+  it('#getPercentages should have a candidate with 100', () => {
+    const input: CandidateInfo[] = [{name: '1', job_count: 2000}, {name: '2', job_count: 0}];
+    // @ts-ignore
+    const resultMap: Map<string, number> = service.getPercentages(input);
+
+    expect(resultMap.get('1')).toEqual(100);
+  });
 });
