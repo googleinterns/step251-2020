6bdae7edde992aa77b192ffbebf8aa0f2fd39973
Anca-Maria Roşculete <ancar@google.com>
2020-08-18 07:55:55 +0000

    Add tests for insertAndAdjustPoints function.

--- step-release-vis/src/app/services/environment_test.ts
+++ step-release-vis/src/app/services/environment_test.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { EnvironmentService } from './environment';
+import { EnvironmentService, PolygonUpperBoundYPosition, TimestampUpperBoundSet } from './environment';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CandidateInfo } from '../models/Data';
 
@@ -15,6 +15,47 @@ describe('EnvironmentService', () => {
   });
 
   // TODO(naoai): write getPolygons test
+
+  it('#insertAndAdjustPoints insert new candidate', () => {
+    const inputCandInfo: CandidateInfo[] = [{name: '1', job_count: 100}, {name: '2', job_count: 100}];
+    const inputSet: TimestampUpperBoundSet = new TimestampUpperBoundSet();
+    inputSet.orderMap.set('1', 0);
+    inputSet.snapshot[0] = new PolygonUpperBoundYPosition('1', 100);
+
+    // @ts-ignore
+    const result: [TimestampUpperBoundSet, number] = service.insertAndAdjustPoints(inputCandInfo, inputSet);
+
+    expect(result[0].snapshot[0].position).toEqual(50);
+    expect(result[0].snapshot[1].position).toEqual(100);
+    expect(result[1]).toEqual(1);
+  });
+
+  it('#insertAndAdjustPoints candidateInfo[] empty', () => {
+    const emptyCandInfo: CandidateInfo[] = [];
+    const inputSet: TimestampUpperBoundSet = new TimestampUpperBoundSet();
+    inputSet.orderMap.set('1', 0);
+    inputSet.snapshot[0] = new PolygonUpperBoundYPosition('1', 100);
+
+    // @ts-ignore
+    const result: [TimestampUpperBoundSet, number] = service.insertAndAdjustPoints(emptyCandInfo, inputSet);
+
+    expect(result[0]).toBe(inputSet);
+  });
+
+  it('#insertAndAdjustPoints with empty set', () => {
+    const inputCandInfo: CandidateInfo[] = [{name:  '1', job_count: 105}, {name: '2', job_count: 300},
+      {name: '3', job_count: 595}];
+    const emptySet: TimestampUpperBoundSet = new TimestampUpperBoundSet();
+
+    // @ts-ignore
+    const result: [TimestampUpperBoundSet, number] = service.insertAndAdjustPoints(inputCandInfo, emptySet);
+
+    expect(result[0].snapshot[0].position).toEqual(10.5);
+    expect(result[0].snapshot[1].position).toEqual(40.5);
+    expect(result[0].snapshot[2].position).toEqual(100);
+    expect(result[1]).toEqual(3);
+  });
+
   it('#getPercentages should return 3 candidates with ~ equal percentages', () => {
     const input: CandidateInfo[] = [{name:  '1', job_count: 105}, {name: '2', job_count: 300},
       {name: '3', job_count: 595}];
