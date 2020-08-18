f7abb853d02f9c45e07ce04247026ecd3818a240
Andrey Starodubtsev <andreystar2403@gmail.com>
2020-08-18 17:40:20 +0300

    Fix module imports.

--- step-release-vis/src/app/components/home/home_test.ts
+++ step-release-vis/src/app/components/home/home_test.ts
@@ -1,6 +1,7 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {HomeComponent} from './home';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -9,6 +10,7 @@ describe('HomeComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [HomeComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
   }));
 
