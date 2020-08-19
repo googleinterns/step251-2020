b85f0fa52dbf8571881c3f1f4adbe98ca9ddc2c8
Andrey Starodubtsev <andreystar2403@gmail.com>
2020-08-19 16:53:50 +0300

    Add more polygons to stub.

--- step-release-vis/src/testing/EnvironmentServiceStub.ts
+++ step-release-vis/src/testing/EnvironmentServiceStub.ts
@@ -24,6 +24,54 @@ export class EnvironmentServiceStub {
       ],
       this.candName
     ),
+    new Polygon(
+      [
+        {x: 100, y: 0},
+        {x: 100, y: 100},
+        {x: 200, y: 0},
+      ],
+      this.candName
+    ),
+    new Polygon(
+      [
+        {x: 100, y: 100},
+        {x: 200, y: 100},
+        {x: 200, y: 0},
+      ],
+      this.candName
+    ),
+    new Polygon(
+      [
+        {x: 200, y: 0},
+        {x: 200, y: 100},
+        {x: 300, y: 0},
+      ],
+      this.candName
+    ),
+    new Polygon(
+      [
+        {x: 200, y: 100},
+        {x: 300, y: 100},
+        {x: 300, y: 0},
+      ],
+      this.candName
+    ),
+    new Polygon(
+      [
+        {x: 300, y: 0},
+        {x: 300, y: 100},
+        {x: 400, y: 0},
+      ],
+      this.candName
+    ),
+    new Polygon(
+      [
+        {x: 300, y: 100},
+        {x: 400, y: 100},
+        {x: 400, y: 0},
+      ],
+      this.candName
+    ),
   ];
 
   getPolygons(jsonFile: string): Observable<Polygon[]> {
