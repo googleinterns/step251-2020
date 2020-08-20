a540e50ab4348104868ba364e055fd57ee9bada9
Andrey Starodubtsev <andreystar2403@gmail.com>
2020-08-20 14:06:00 +0300

    Add test for FileService.

--- step-release-vis/src/app/services/file_test.ts
+++ step-release-vis/src/app/services/file_test.ts
@@ -1,21 +1,37 @@
 import {TestBed} from '@angular/core/testing';
 
 import {FileService} from './file';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 describe('FileService', () => {
   let service: FileService;
+  const testRequest = {
+    filePath: './test',
+    response: 'test_response',
+  };
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(FileService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  // TODO(andreystar): add test for this service
+  it('should read existing file', () => {
+    service
+      .readContents(testRequest.filePath)
+      .subscribe(contents => expect(contents).toEqual(testRequest.response));
+    const req = httpMock.expectOne(testRequest.filePath);
+    expect(req.request.method).toBe('GET');
+    req.flush(testRequest.response);
+  });
 });
