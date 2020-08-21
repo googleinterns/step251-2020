b6141bfc01a9fa3cea2451b447770d330d017c3b
Anca-Maria Roşculete <ancar@google.com>
2020-08-21 14:48:05 +0000

    Add CandidateService.

---
+++ step-release-vis/src/app/services/candidate_test.ts
@@ -0,0 +1,16 @@
+import {TestBed} from '@angular/core/testing';
+
+import {CandidateService} from './candidate';
+
+describe('CandidateService', () => {
+  let service: CandidateService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CandidateService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
