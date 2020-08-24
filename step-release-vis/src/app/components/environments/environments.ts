f828ff98579260ccf563b54186415c7b274589e1
Andrey Starodubtsev <andreystar2403@gmail.com>
2020-08-24 14:01:08 +0300

    Utilize CandidateService in parent component.

--- step-release-vis/src/app/components/environments/environments.ts
+++ step-release-vis/src/app/components/environments/environments.ts
@@ -4,6 +4,7 @@ import {FileService} from '../../services/file';
 import {ParamService} from '../../services/param';
 import {ActivatedRoute} from '@angular/router';
 import {CandidateService} from '../../services/candidate';
+import {shuffle} from 'lodash';
 
 @Component({
   selector: 'app-environments',
@@ -36,9 +37,48 @@ export class EnvironmentsComponent implements OnInit {
     this.envWidth = window.innerWidth;
     this.envHeight = window.innerHeight / environments.length;
     this.environments = environments;
+    const candNames = new Set<string>();
     for (const environment of this.environments) {
       for (const snapshot of environment.snapshots) {
+        for (const candsInfo of snapshot.cands_info) {
+          candNames.add(candsInfo.name);
+        }
       }
     }
+    const shuffledIndices = shuffle(this.increasingSequence(0, candNames.size));
+    [...candNames].forEach((name, index) => {
+      const color = this.getHue(shuffledIndices[index], candNames.size);
+      this.candidateService.addCandidate(color, name);
+    });
+  }
+
+  /**
+   * Generates a hue for an HSL color by splitting the range 0..360 into
+   * `amount` number of chunks.
+   *
+   * e.g. amount = 4
+   * |--0--|--1--|--2--|--3--|
+   * 0                      360
+   *
+   * @param index the index of the color
+   * @param amount amount of colors
+   */
+  private getHue(index: number, amount: number): number {
+    const chunkSize = 360 / amount;
+    return Math.floor(chunkSize * index + chunkSize / 2);
+  }
+
+  /**
+   * Returns an array of `length` numbers, starting from `start` and increasing by one.
+   *
+   * @param start the first element of the sequence
+   * @param length amount of elements
+   */
+  private increasingSequence(start: number, length: number): number[] {
+    const res: number[] = [];
+    for (let i = start; i < start + length; i++) {
+      res.push(i);
+    }
+    return res;
   }
 }
