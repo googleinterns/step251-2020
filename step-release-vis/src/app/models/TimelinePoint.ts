77b5d5a59a656dda4d008bbfdd72b72ca45dd429
Andrey Starodubtsev <andreystar2403@gmail.com>
2020-08-26 16:11:54 +0300

    Add TimelinePoint model.

---
+++ step-release-vis/src/app/models/TimelinePoint.ts
@@ -0,0 +1,14 @@
+export class TimelinePoint {
+  timestamp: number;
+  x: number;
+  timestampString: string;
+
+  constructor(timestamp: number, x: number) {
+    this.timestamp = timestamp;
+    this.x = x;
+    this.timestampString = new Date(this.timestamp * 1000).toLocaleString(
+      'en-GB'
+    );
+    console.log(this.timestampString);
+  }
+}
