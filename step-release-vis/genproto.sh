3abf995ffb0e38fa12ade00ab7f5d641f247ed6e
Anca-Maria Roşculete <ancar@google.com>
2020-08-31 16:56:50 +0000

    Fix path.

--- step-release-vis/genproto.sh
+++ step-release-vis/genproto.sh
@@ -1,6 +1,6 @@
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 PROTO_PATH="./src/app/proto"
-PROTOC_OUT_DIR="src/app/proto/generated"
+PROTOC_OUT_DIR="./src/app/proto/generated"
 ./node_modules/protoc/protoc/bin/protoc \
        --proto_path=${PROTO_PATH} \
        --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
