724a4c646be24489f961fd424d63861ea752fc0b
Anca-Maria Roşculete <ancar@google.com>
2020-08-31 16:54:10 +0000

    Add full path to protoc.

--- step-release-vis/genproto.sh
+++ step-release-vis/genproto.sh
@@ -1,7 +1,7 @@
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 PROTO_PATH="./src/app/proto"
 PROTOC_OUT_DIR="src/app/proto/generated"
-protoc \
+./node_modules/protoc/protoc/bin/protoc \
        --proto_path=${PROTO_PATH} \
        --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
        --js_out="import_style=commonjs,binary:${PROTOC_OUT_DIR}" \
