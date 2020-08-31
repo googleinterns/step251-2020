b1af564a5cf2c10b6e0a8f614afcc79818779613
Laura Georgescu <naoai@google.com>
2020-08-31 12:37:08 +0000

    Add class doc and other comments.

--- step-release-vis/src/app/models/SnapshotInterval.ts
+++ step-release-vis/src/app/models/SnapshotInterval.ts
@@ -1,5 +1,9 @@
 import {Snapshot} from '../models/Data';
 
+/*
+  Used for describing which positions of the mouse corelate to which snapshot to be displayed.
+  If the mouse is in [start, end] on the X axis, show details about this snapshot in the tooltip.
+*/
 export class SnapshotInterval {
   start: number;
   end: number;
