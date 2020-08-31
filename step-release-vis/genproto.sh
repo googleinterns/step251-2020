971a1f0df460b6cb165d6be3e22c4d7d9584b30c
Anca-Maria Roşculete <ancar@google.com>
2020-08-31 16:00:39 +0000

    Create script for creating the generated files.

---
+++ step-release-vis/genproto.sh
@@ -0,0 +1,9 @@
+PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
+PROTO_PATH="./src/app/proto"
+PROTOC_OUT_DIR="src/app/proto/generated"
+protoc \
+       --proto_path=${PROTO_PATH} \
+       --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
+       --js_out="import_style=commonjs,binary:${PROTOC_OUT_DIR}" \
+       --ts_out="import_style=commonjs,binary:${PROTOC_OUT_DIR}" \
+       data.proto
