8cc05a66c652b3927623af8542ed76973d36a87b
Anca-Maria Roşculete <ancar@google.com>
2020-09-01 14:26:32 +0000

    Make already existing tests and components work.

--- step-release-vis/src/app/components/tooltip/tooltip.ts
+++ step-release-vis/src/app/components/tooltip/tooltip.ts
@@ -11,7 +11,7 @@ export class TooltipComponent implements OnInit {
   currentSnapshotIndex: number;
   snapshotIntervals: SnapshotInterval[];
 
-  @Input() tooltip: Tooltip;
+  @Input() tooltip: Tooltip = new Tooltip();
 
   candidateName: string;
 
@@ -36,19 +36,25 @@ export class TooltipComponent implements OnInit {
   // computes the left position of the tooltip according to the mouse's X position
   getLeft(): string {
     const divTooltip = document.getElementById(this.tooltip.envName);
-    if (this.tooltip.mouseX + divTooltip.offsetWidth > window.innerWidth) {
-      return this.tooltip.mouseX - divTooltip.offsetWidth + 'px';
+    if (divTooltip != null) {
+      if (this.tooltip.mouseX + divTooltip.offsetWidth > window.innerWidth) {
+        return this.tooltip.mouseX - divTooltip.offsetWidth + 'px';
+      }
+      return this.tooltip.mouseX + 20 + 'px';
     }
-    return this.tooltip.mouseX + 20 + 'px';
+    return '';
   }
 
   // computes the top position of the tooltip according to the mouse's Y position
   getTop(): string {
     const divTooltip = document.getElementById(this.tooltip.envName);
-    if (this.tooltip.mouseY + divTooltip.offsetHeight > window.innerHeight) {
-      return this.tooltip.mouseY - divTooltip.offsetHeight + 'px';
+    if (divTooltip != null) {
+      if (this.tooltip.mouseY + divTooltip.offsetHeight > window.innerHeight) {
+        return this.tooltip.mouseY - divTooltip.offsetHeight + 'px';
+      }
+      return this.tooltip.mouseY + 20 + 'px';
     }
-    return this.tooltip.mouseY + 20 + 'px';
+    return '';
   }
 
   // make the tooltip visible or not
