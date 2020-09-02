dde5a029a1c0e7c4220c45df892a9a0be2d6edbe
Anca-Maria Roşculete <ancar@google.com>
2020-09-02 13:13:20 +0000

    Update style with the DOM.

--- step-release-vis/src/app/components/tooltip/tooltip.ts
+++ step-release-vis/src/app/components/tooltip/tooltip.ts
@@ -11,8 +11,8 @@ import {CandidateService} from '../../services/candidateService';
 export class TooltipComponent implements OnInit {
   @Input() tooltip: Tooltip = new Tooltip();
   currentSnapshot: Snapshot;
-  width: number;
-  height: number;
+  width = 200;
+  height = 50;
 
   // TODO(#234): Compute width/height of the tooltip according to the data.
 
@@ -70,6 +70,7 @@ export class TooltipComponent implements OnInit {
     const localTimeZone: string = Intl.DateTimeFormat().resolvedOptions()
       .timeZone;
     // TODO(#210): add other details
+    this.updateStyle();
     return dateTime.toLocaleString('en-GB') + ' ' + localTimeZone;
   }
 
@@ -103,6 +104,26 @@ export class TooltipComponent implements OnInit {
       return 'block';
     } else {
       return 'none';
+    }
+  }
+
+  getWidth(): string {
+    return this.width + 'px';
+  }
+
+  getHeight(): string {
+    return this.height + 'px';
+  }
+
+  private updateStyle(): void {
+    const tooltipElement = document.getElementById(this.tooltip.envName);
+    if (tooltipElement) {
+      const style = tooltipElement.style;
+      style.width = this.getWidth();
+      style.height = this.getHeight();
+      style.left = this.getLeft();
+      style.top = this.getTop();
+      style.display = this.getShow();
     }
   }
 }
