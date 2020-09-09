55db16e6cdfba9bbd58dd2a5b1f0af0cd8853751
Laura Georgescu <naoai@google.com>
2020-09-09 09:51:27 +0000

    Construct graph.

--- step-release-vis/src/app/services/coloringService.ts
+++ step-release-vis/src/app/services/coloringService.ts
@@ -6,20 +6,73 @@ import {CandidateService} from './candidateService';
   providedIn: 'root',
 })
 export class ColoringService {
+  /*
+   * Colors candidates by looking at the pairs of candidates appearing the most next to each other
+   * and coloring the candidates iteratively.
+   * O(edges * log edges + candidates)
+   */
+  private noOfCandidates: number;
+  private candNames: Set<string>;
+  candidateGraph: Map<string, string[]> = new Map(); // what candidates share sides?
+  private colorOf: Map<string, number> = new Map(); // what index has the color which is assigned to the candidate?
+  edgeOccurrences: Map<string, number> = new Map(); // how many sides do candidates share?
   colorsComputed = false;
-  private edgeOccurrences: Map<string, number> = new Map();
 
   constructor(private candidateService: CandidateService) {}
 
-  colorCandidates(edges: Map<string, number>): void {
-    // TODO(#271): implement this
+  colorCandidates(edges: Map<string, number>, candNames: Set<string>): void {
+    this.candidateGraph.clear();
+    this.colorOf.clear();
+    this.edgeOccurrences.clear();
     this.colorsComputed = false;
-    this.initialize(edges);
+
+    this.initialize(edges, candNames);
     this.assignColors();
   }
 
-  private initialize(edges: Map<string, number>): void {
-    // TODO(#271)
+  initialize(edges: Map<string, number>, candNames: Set<string>): void {
+    this.candNames = candNames;
+    this.noOfCandidates = this.candNames.size;
+    this.constructGraph(edges);
+    this.colorsComputed = false;
+  }
+
+  /* Add y to the list of neighbours of x */
+  private addEdgeToGraph(x: string, y: string): void {
+    let neighbours: string[] = [];
+    if (this.candidateGraph.has(x)) {
+      neighbours = this.candidateGraph.get(x);
+    }
+    if (neighbours.includes(y) === false) {
+      neighbours.push(y);
+    }
+    this.candidateGraph.set(x, neighbours);
+  }
+
+  /* Sets edges and constructs graph */
+  private constructGraph(edges: Map<string, number>): void {
+    for (const edgeKeyValue of edges) {
+      this.edgeOccurrences.set(edgeKeyValue[0], edgeKeyValue[1]);
+
+      const edge: CandidateEdge = CandidateEdge.fromKey(edgeKeyValue[0]);
+
+      this.addEdgeToGraph(edge.candidate1, edge.candidate2);
+      this.addEdgeToGraph(edge.candidate2, edge.candidate1);
+    }
+  }
+
+  /* Splits the color palette like so
+   * |--0--|--1--|--2--|--3--|
+   * 0                      360
+   */
+  private selectAssignableColors(): number[] {
+    const chunkSize = 360 / this.noOfCandidates;
+    const colors: number[] = [];
+    for (let index = 0; index < this.noOfCandidates; index++) {
+      const color = chunkSize * index + chunkSize / 2;
+      colors.push(color);
+    }
+    return colors;
   }
 
   /* Decides on the colors for all candidates */
@@ -52,6 +105,10 @@ export class CandidateEdge {
   constructor(x: string, y: string) {
     this.candidate1 = x;
     this.candidate2 = y;
+  }
+
+  static fromKey(key: string): CandidateEdge {
+    return new CandidateEdge('a', 'b');
   }
 
   toKey(): string {
