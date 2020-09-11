ddcef7f6984ac6627187e62e5b6974945fc9e1d6
Anca-Maria Roşculete <ancar@google.com>
2020-09-11 13:28:24 +0000

    Create header component with toggle button for dark mode.

---
+++ step-release-vis/src/app/components/header/header.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {ThemeService} from '../../services/themeService';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.html',
+  styleUrls: ['./header.css'],
+})
+export class HeaderComponent implements OnInit {
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.setTheme();
+  }
+
+  onToggle(): void {
+    this.themeService.theme = !this.themeService.theme;
+    this.themeService.setTheme();
+  }
+
+  getCheck(): boolean {
+    return this.themeService.theme;
+  }
+}
